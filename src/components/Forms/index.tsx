8f10a58384a1bbfce7f6619bb7bf6562aa75b374
Robson Hansen <robsonrudson65@gmail.com>
2025-07-23 00:10:27 -0300

    feat: onChange

--- src/components/Forms/index.tsx
+++ src/components/Forms/index.tsx
@@ -2,16 +2,25 @@ import style from "./styles.module.css";
 import { DefaultInput } from "../DefaultInput";
 import { DefaultButton } from "../DefaultButton";
 import { PlayCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 export function Forms() {
+  const [taskName, setTaskName] = useState("");
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("New task created!");
   }
   return (
     <form onSubmit={handleCreateNewTask} className={style.forms} action="#">
       <div className={style.formsRow}>
-        <DefaultInput id="task" type="text" labelText="Tarefa" placeholder="Adicione uma task" />
+        <DefaultInput
+          id="task"
+          type="text"
+          labelText="Tarefa"
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+          placeholder="Adicione uma Task"
+        />
       </div>
       <div className={style.formsRow}>
         <p>Próximo intervalo em 25min.</p>
